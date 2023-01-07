f5882b94805d71e24793b9a8f20d1202f29250d3
Yuliya Raikova <yuradev2001@gmail.com>
2023-01-07 15:29:10 +0300

    .

--- task1-proxy-pattern/src/main/java/com/mjc/stage2/impl/ProxyConnection.java
+++ task1-proxy-pattern/src/main/java/com/mjc/stage2/impl/ProxyConnection.java
@@ -16,7 +16,7 @@ public class ProxyConnection implements Connection {
 
     @Override
     public void close() {
-        ConnectionPool.getInstance().releaseConnection(realConnection);
+        ConnectionPool.getInstance().releaseConnection(this);
     }
 
     @Override
